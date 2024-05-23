a7df669ad9a2543b887fb5c790d38d580e13cbf2
PM <3749956+CluEleSsUK@users.noreply.github.com>
2024-05-23 09:57:29 +0200

    replaced spinning cube with new logo (#2)
    * replaced spinning cube with new logo
    
    updated favicon
    
    * Optimised images with calibre/image-actions
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,2 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
-import {Canvas} from "@react-three/fiber"
-import {Dice} from "./Dice"
 import "./index.css"
-
-const root = ReactDOM.createRoot(
-    document.getElementById("root") as HTMLElement
-)
-root.render(
-    <React.StrictMode>
-        <a href={"https://github.com/randa-mu"}>
-            <Canvas>
-                <Dice/>
-            </Canvas>
-        </a>
-    </React.StrictMode>
-)
 
